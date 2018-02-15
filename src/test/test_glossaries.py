911a8200c58f3e8abfef78f5284262b7e063b0a9
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-02-15 19:50:46 +0300

    style: move source files to src dir

--- src/test/test_glossaries.py
+++ src/test/test_glossaries.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import inspect
 import unittest
 import mock
 
-import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
