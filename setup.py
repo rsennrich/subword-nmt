1df7c24a371156d03cb604e050bc03f8623a8fbb
Rico Sennrich <rico.sennrich@gmx.ch>
2018-05-17 13:28:21 +0100

    proper markdown on PyPi

--- setup.py
+++ setup.py
@@ -11,14 +11,23 @@ def test_suite():
 
 setup(
     name='subword_nmt',
-    version='0.3.1',
+    version='0.3.2',
     description='Unsupervised Word Segmentation for Neural Machine Translation and Text Generation',
-    long_description= (codecs.open("README.md", encoding='utf-8').read() +
+    long_description=(codecs.open("README.md", encoding='utf-8').read() +
                       "\n\n" + codecs.open("CHANGELOG.md", encoding='utf-8').read()),
+    long_description_content_type="text/markdown",
     url='https://github.com/rsennrich/subword-nmt',
     author='Rico Sennrich',
     license='MIT',
     test_suite='setup.test_suite',
+    classifiers=[
+    'Intended Audience :: Developers',
+    'Topic :: Text Processing',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    ],
     packages=find_packages(),
     entry_points={
         'console_scripts': ['subword-nmt=subword_nmt.subword_nmt:main'],
